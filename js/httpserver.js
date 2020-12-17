567439f11fbdb49dc3b46d0b973f51f02b080dac
tbottmled <migfly@sina.cn>
2020-12-17 22:43:12 +0800

    添加http post函数

--- js/httpserver.js
+++ js/httpserver.js
@@ -19,4 +19,18 @@ function HttpGetRequest(url){
 }
 
 //发送http post请求
+function HttpPostRequest(url,requestData){
+    request({
+        url: url,
+        method: "POST",
+        json: true,
+        headers: {
+            "content-type": "application/json",
+        },
+        body: JSON.stringify(requestData)
+    }, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+        }
+    });
+}
 
